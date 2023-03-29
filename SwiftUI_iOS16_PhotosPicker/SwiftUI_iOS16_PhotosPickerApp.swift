a83059b320eba5078d8b366e73f7a4a6a310f947
Mladen Mikič <mladen.mikic@unseenexcellence.com>
2023-03-29 07:10:34 +0200

    Initial Commit

---
+++ SwiftUI_iOS16_PhotosPicker/SwiftUI_iOS16_PhotosPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iOS16_PhotosPickerApp.swift
+//  SwiftUI_iOS16_PhotosPicker
+//
+//  Created by Mladen Mikić on 29.03.2023..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iOS16_PhotosPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
