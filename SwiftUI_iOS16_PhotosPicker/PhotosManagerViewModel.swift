9c9b442885958115b0b51c0a73732095dd4a2a5a
Mladen Mikič <mladen.mikic@unseenexcellence.com>
2023-03-31 07:00:31 +0200

    Update - added publishers (which dont work)

--- SwiftUI_iOS16_PhotosPicker/PhotosManagerViewModel.swift
+++ SwiftUI_iOS16_PhotosPicker/PhotosManagerViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 
 final class PhotosManagerViewModel: ObservableObject {
@@ -13,8 +14,43 @@ final class PhotosManagerViewModel: ObservableObject {
     @Published var imagesPVM: PhotosViewModel
     @Published var videosPVM: PhotosViewModel
     
+    private var bag = Set<AnyCancellable>()
+    
+    // MARK: - Init.
+    
     init() {
         self.imagesPVM = .init(maxSelectionCount: nil, filter: .images)
         self.videosPVM = .init(maxSelectionCount: 1, filter: .videos)
+    }
+    
+    private func setupPublishers() {
+        
+        self.$imagesPVM
+            .receive(on: RunLoop.main)
+            .sink { _ in
+                print("imagesPVM changed")
+            }
+            .store(in: &self.bag)
+        
+        self.$videosPVM
+            .receive(on: RunLoop.main)
+            .sink { _ in
+                print("imagesPVM changed")
+            }
+            .store(in: &self.bag)
+        
+        self.imagesPVM.$selectedItems
+            .receive(on: RunLoop.main)
+            .sink { newSelectedItems in
+                print("New images have been selected: \(newSelectedItems)")
+            }
+            .store(in: &self.bag)
+                
+        self.videosPVM.$selectedItems
+            .receive(on: RunLoop.main)
+            .sink { newSelectedItems in
+                print("New videos have been selected: \(newSelectedItems)")
+            }
+            .store(in: &self.bag)
     }
 }
