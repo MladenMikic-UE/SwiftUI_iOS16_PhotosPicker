635ff075436ac49ac2300800e0007d82f65c769b
Mladen Mikič <mladen.mikic@unseenexcellence.com>
2023-03-31 06:39:58 +0200

    Update - added docu comments, viewmodels and some views for testing

--- SwiftUI_iOS16_PhotosPicker/ContentView.swift
+++ SwiftUI_iOS16_PhotosPicker/ContentView.swift
@@ -2,20 +2,86 @@
 //  ContentView.swift
 //  SwiftUI_iOS16_PhotosPicker
 //
-//  Created by Mladen Mikić on 29.03.2023..
+//  Created by Mladen Mikić on 29.03.2023.
 //
 
 import SwiftUI
+import PhotosUI
+
 
 struct ContentView: View {
-    var body: some View {
+    
+    @StateObject var vm: PhotosManagerViewModel = .init()
+    
+        var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            PhotosPickerContainerView(viewModel: vm.imagesPVM) {
+                AnyView(
+                    Image(systemName: "photo.on.rectangle.angled")
+                )
+            }
+            
+            PhotosPickerContainerView(viewModel: vm.videosPVM) {
+                AnyView(
+                    Button(action: {
+                        let _ = print("Button2")
+                    }, label: {
+                        Text("Button2")
+                    })
+                    .allowsHitTesting(false)
+                )
+            }
+
         }
-        .padding()
+    }
+    
+
+}
+
+
+
+struct PhotosPickerContainerView: View {
+    
+    @ObservedObject var viewModel: PhotosViewModel
+    // A Button can not be used. The touch is not passed to the PhotosPicker.
+    // Using .allowsHitTesting(false) on the button does not make much sense but it works.
+    var photosButtonView: () -> AnyView
+    
+    var body: some View {
+        
+        PhotosPicker(selection: $viewModel.selectedItems,
+                     maxSelectionCount: viewModel.maxSelectionCount,
+                     matching: viewModel.filter, label: { photosButtonView() })
+        
+    }
+}
+
+
+final class PhotosViewModel: ObservableObject {
+
+    @Published var selectedItems: [PhotosPickerItem]
+    /// Setting the maxSelectionCount to 1 will set single selection.
+    let maxSelectionCount: Int?
+    let filter: PHPickerFilter
+    
+    public init(maxSelectionCount: Int?,
+                filter: PHPickerFilter) {
+        self.selectedItems = []
+        self.maxSelectionCount = maxSelectionCount
+        self.filter = filter
+    }
+}
+
+final class PhotosManagerViewModel: ObservableObject {
+    
+    @Published var imagesPVM: PhotosViewModel
+    @Published var videosPVM: PhotosViewModel
+    
+    init() {
+        self.imagesPVM = .init(maxSelectionCount: nil, filter: .images)
+        self.videosPVM = .init(maxSelectionCount: 1, filter: .videos)
     }
 }
 
--- SwiftUI_iOS16_PhotosPicker/SwiftUI_iOS16_PhotosPickerApp.swift
+++ SwiftUI_iOS16_PhotosPicker/SwiftUI_iOS16_PhotosPickerApp.swift
@@ -2,10 +2,19 @@
 //  SwiftUI_iOS16_PhotosPickerApp.swift
 //  SwiftUI_iOS16_PhotosPicker
 //
-//  Created by Mladen Mikić on 29.03.2023..
+//  Created by Mladen Mikić on 29.03.2023.
 //
 
 import SwiftUI
+
+// Documenations:
+// Apple:
+// - Sample project
+// https://developer.apple.com/documentation/photokit/bringing_photos_picker_to_your_swiftui_app
+// - WWDC
+// https://developer.apple.com/videos/play/wwdc2022/10023/
+// - Docu page:
+// https://developer.apple.com/documentation/photokit/photospicker
 
 @main
 struct SwiftUI_iOS16_PhotosPickerApp: App {
