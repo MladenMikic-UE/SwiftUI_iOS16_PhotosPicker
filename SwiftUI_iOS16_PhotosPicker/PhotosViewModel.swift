b8b4699c4d522ce0be54b1c71271dc34bb9dad3b
Mladen Mikič <mladen.mikic@unseenexcellence.com>
2023-04-12 06:58:22 +0200

    Update - added conversion method for loaded image and transfer method for image conversion

--- SwiftUI_iOS16_PhotosPicker/PhotosViewModel.swift
+++ SwiftUI_iOS16_PhotosPicker/PhotosViewModel.swift
@@ -15,6 +15,8 @@ final class PhotosViewModel: ObservableObject {
     /// Temporary selections allows for items to be selected and manipulated further before exposing the selection array.
     @Published var tmpSelectedItems: [PhotosPickerItem]
     @Published var selectedItems: [PhotosPickerItem]
+    @Published var selectedImages: [UIImage]
+    @Published var lastSelectionError: Error? = nil
     /// Setting the maxSelectionCount to 1 will set single selection.
     let maxSelectionCount: Int?
     let filter: PHPickerFilter
@@ -27,6 +29,7 @@ final class PhotosViewModel: ObservableObject {
                 filter: PHPickerFilter) {
         self.selectedItems = []
         self.tmpSelectedItems = []
+        self.selectedImages = []
         self.maxSelectionCount = maxSelectionCount
         self.filter = filter
         
@@ -39,11 +42,47 @@ final class PhotosViewModel: ObservableObject {
                 if !self.selectedItems.isEmpty {
                     // Reselection will trigger cleanup
                     self.selectedItems = []
+                    self.lastSelectionError = nil
+                    self.selectedImages = []
                 }
                 
-                self.tmpSelectedItems = []
+                self.tmpSelectedItems = []         
+                
                 self.selectedItems = newSelectedItems
             }
             .store(in: &self.bag)
+    }
+    
+    private func loadTransferable(from imageSelection: PhotosPickerItem) -> Progress {
+        return imageSelection.loadTransferable(type: WrapImage.self) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let wrapImage?):
+                    self.selectedImages.append(wrapImage.image)
+                case .success(nil):
+                    self.lastSelectionError = TransferError.loadFailed
+                case .failure(let error):
+                    self.lastSelectionError = error
+                }
+            }
+        }
+    }
+}
+
+enum TransferError: Error {
+    case importFailed
+    case loadFailed
+}
+
+struct WrapImage: Transferable {
+    let image: UIImage
+    
+    static var transferRepresentation: some TransferRepresentation {
+        DataRepresentation(importedContentType: .image) { data in
+            guard let uiImage = UIImage(data: data) else {
+                throw TransferError.importFailed
+            }
+            return WrapImage(image: uiImage)
+        }
     }
 }
